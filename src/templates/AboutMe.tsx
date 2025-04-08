5bb097b712a1bb3ff4fdb925b00c1a94d9af25ec
amacierzynski <macierzynski.adam@gmail.com>
2025-04-08 22:21:57 +0200

    fix: import order

--- src/templates/AboutMe.tsx
+++ src/templates/AboutMe.tsx
@@ -1,6 +1,6 @@
+import Image from 'next/image';
 import { Section } from '../layout/Section';
 import { Background } from '../background/Background';
-import Image from 'next/image';
 
 const AboutMe = () => (
   <Background color="bg-white">
